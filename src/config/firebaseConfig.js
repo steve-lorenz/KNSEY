80abb6539a484149620458700d9e4288ae2ac08d
Steve Lorenz <lorenz21@hawaii.edu>
2022-04-24 16:08:05 -0700

    Update firebase and react-spinners

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,14 +1,14 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore'
-import 'firebase/auth/'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const config = {
-   apiKey: process.env.REACT_APP_FIREBASE_API,
-   authDomain: "knsey-bce8e.firebaseapp.com",
-   databaseURL: "https://knsey-bce8e.firebaseio.com",
-   projectId: "knsey-bce8e",
-   storageBucket: "knsey-bce8e.appspot.com",
-   messagingSenderId: "923274980719"
+  apiKey: process.env.REACT_APP_FIREBASE_API,
+  authDomain: 'knsey-bce8e.firebaseapp.com',
+  databaseURL: 'https://knsey-bce8e.firebaseio.com',
+  projectId: 'knsey-bce8e',
+  storageBucket: 'knsey-bce8e.appspot.com',
+  messagingSenderId: '923274980719',
 };
 
 firebase.initializeApp(config);
